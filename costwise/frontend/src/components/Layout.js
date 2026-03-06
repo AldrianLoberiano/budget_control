0e3f999291aeef4fa3c7ffed01a8386efc9d0753
AldrianLoberiano <loberianorian@gmail.com>
2026-03-07 01:17:44 +0800

    Update Layout.js

--- costwise/frontend/src/components/Layout.js
+++ costwise/frontend/src/components/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import AppHeader from './AppHeader';
 
 const Layout = ({ children }) => {
   return (
     <div className="app-layout">
       <Sidebar />
-      <main className="main-content">{children}</main>
+      <div className="main-wrapper">
+        <AppHeader />
+        <main className="main-content">{children}</main>
+      </div>
     </div>
   );
 };
