ffcabb04cc9f26e15346780ce6780d382284a49f
AldrianLoberiano <loberianorian@gmail.com>
2026-03-06 20:58:50 +0800

    Create Layout.js

---
+++ costwise/frontend/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <main className="main-content">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
